62a353ca48703fd407392565b7937c105aa83c6f
Gil Alpert <alpertgil@gmail.com>
2025-01-30 21:32:15 +0200

    refactor WinApi namespace to IWaitable interface

---
+++ Framework/Include/Synchronization/IWaitable.hpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "Utils/Time.hpp"
+
+#include <cstdint>
+#include <optional>
+
+enum class WaitStatus : uint32_t
+{
+	FINISHED = 0,
+	OBJECT_CLOSED,
+	TIMEOUT,
+	FAILED,
+};
+
+struct WaitResult final
+{
+	WaitStatus status;
+	std::optional<uint32_t> triggered_object;
+};
+
+class IWaitable
+{
+public:
+	explicit IWaitable() = default;
+	virtual ~IWaitable() = default;
+	IWaitable(const IWaitable&) = delete;
+	IWaitable& operator=(const IWaitable&) = delete;
+	IWaitable(IWaitable&&) = delete;
+	IWaitable& operator=(IWaitable&&) = delete;
+
+	[[nodiscard]] virtual HANDLE handle() const = 0;
+
+	[[nodiscard]] WaitStatus wait(Time::Duration timeout) const;
+
+	static void sleep(Time::Duration duration);
+};
---
+++ Framework/Synchronization/IWaitable.cpp
@@ -0,0 +1,32 @@
+﻿#include "Synchronization/IWaitable.hpp"
+
+static WaitStatus to_wait_status(const DWORD result)
+{
+	switch (result)
+	{
+	case WAIT_OBJECT_0:
+		return WaitStatus::FINISHED;
+
+	case WAIT_ABANDONED:
+		return WaitStatus::OBJECT_CLOSED;
+
+	case WAIT_TIMEOUT:
+		return WaitStatus::TIMEOUT;
+
+	case WAIT_FAILED:
+		[[fallthrough]];
+	default:
+		return WaitStatus::FAILED;
+	}
+}
+
+WaitStatus IWaitable::wait(const Time::Duration timeout) const
+{
+	const DWORD result = WaitForSingleObject(handle(), timeout.count());
+	return to_wait_status(result);
+}
+
+void IWaitable::sleep(const Time::Duration duration)
+{
+	Sleep(Time::duration_cast<Time::Millis>(duration).count());
+}
