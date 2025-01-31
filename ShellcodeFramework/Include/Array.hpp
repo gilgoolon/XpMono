e877a2198d770295850c124505a686eab79d3d21
Gil Alpert <alpertgil@gmail.com>
2025-01-31 10:20:34 +0200

    templated Array<T> class

--- ShellcodeFramework/Include/Array.hpp
+++ ShellcodeFramework/Include/Array.hpp
@@ -1,4 +1,7 @@
 ﻿#pragma once
+#include "HeapMemory.hpp"
+
+#include <cstdint>
 
 namespace Shellcode
 {
@@ -6,19 +9,28 @@ template <typename T>
 class Array final
 {
 public:
-	explicit Array();
-	~Array();
+	explicit Array(const uint32_t size, bool& result) :
+		m_memory(size * sizeof(T), HeapMemory::Permissions::READ_WRITE, result)
+	{
+	}
+
+	~Array() = default;
 	Array(const Array&) = delete;
 	Array& operator=(const Array&) = delete;
 	Array(Array&&) = delete;
 	Array& operator=(Array&&) = delete;
 
-private:
-	T*
-};
+	T& operator[](const uint32_t index)
+	{
+		return const_cast<T&>(const_cast<const Array*>(this)->operator[](index));
+	}
 
-template <typename T>
-Array<T>::Array()
-{
-}
+	const T& operator[](const uint32_t index) const
+	{
+		return static_cast<const T*>(m_memory.get())[index];
+	}
+
+private:
+	HeapMemory m_memory;
+};
 }
