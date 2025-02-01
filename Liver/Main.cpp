07ba29a4bfb62b05d6ee057453a4e971313735bb
Gil Alpert <alpertgil@gmail.com>
2025-02-01 10:56:55 +0200

    create new Liver project for main worker

---
+++ Liver/Main.cpp
@@ -0,0 +1,56 @@
+﻿#include "ApricotException.hpp"
+#include "ApricotLibrary.hpp"
+#include "Exception.hpp"
+#include "Trace.hpp"
+#include "Filesystem/File.hpp"
+
+#include <Windows.h>
+
+static void main_logic();
+
+int WINAPI wWinMain([[maybe_unused]] HINSTANCE hInstance,
+                    [[maybe_unused]] HINSTANCE hPrevInstance,
+                    [[maybe_unused]] PWSTR pCmdLine,
+                    [[maybe_unused]] int nCmdShow)
+{
+	try
+	{
+		main_logic();
+		return EXIT_SUCCESS;
+	}
+	catch (const ApricotException& ex)
+	{
+		TRACE(
+			"uncaught ApricotException with code ",
+			ex.code(),
+			" and ApricotCode ",
+			static_cast<uint32_t>(ex.apricot_code())
+		)
+	}
+	catch (const WinApiException& ex)
+	{
+		TRACE("uncaught WinApiException with code ", ex.code(), " and error ", ex.error())
+	}
+	catch (const Exception& ex)
+	{
+		TRACE("uncaught Exception with code ", ex.code())
+	}
+	catch (const std::exception& ex)
+	{
+		TRACE("uncaught std::exception: ", ex.what())
+	}
+	catch (...)
+	{
+		TRACE("uncaught unknown or critical exception")
+	}
+	return EXIT_SUCCESS;
+}
+
+static void main_logic()
+{
+	ApricotLibrary lib(
+		File(LR"(C:\Users\alper\OneDrive\Documents\XpMono\Apricot\Debug\SimpleDll.dll)", File::Mode::READ).read()
+	);
+	using SimpleFunction = void(*)();
+	lib.call<SimpleFunction>(1);
+}
