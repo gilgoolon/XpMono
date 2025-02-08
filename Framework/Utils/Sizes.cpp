4c09a6330aa04ecfc5d6444d4b77aaee04903435
Gil Alpert <alpertgil@gmail.com>
2025-02-08 22:30:52 +0200

    fix make_large_int

--- Framework/Utils/Sizes.cpp
+++ Framework/Utils/Sizes.cpp
@@ -2,5 +2,5 @@
 
 uint64_t Sizes::make_large_int(const uint32_t low_part, const uint32_t high_part)
 {
-	return (static_cast<uint64_t>(high_part) << 32) & low_part;
+	return (static_cast<uint64_t>(high_part) << 32) | low_part;
 }
