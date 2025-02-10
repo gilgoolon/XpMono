fc38c24754630ddc215113bfa742d5955310744f
Gil Alpert <alpertgil@gmail.com>
2025-02-10 20:47:28 +0200

    implement FullFilesystemIterator for iteration over all partitions

---
+++ Framework/Include/Filesystem/FullFilesystemIterator.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "RecursiveFileIterator.hpp"
+#include "VolumeIterator.hpp"
+
+#include <memory>
+
+class FullFilesystemIterator final
+{
+public:
+	explicit FullFilesystemIterator();
+	~FullFilesystemIterator() = default;
+	FullFilesystemIterator(const FullFilesystemIterator&) = delete;
+	FullFilesystemIterator& operator=(const FullFilesystemIterator&) = delete;
+	FullFilesystemIterator(FullFilesystemIterator&&) = delete;
+	FullFilesystemIterator& operator=(FullFilesystemIterator&&) = delete;
+
+	[[nodiscard]] bool has_next() const;
+	[[nodiscard]] std::unique_ptr<FileEntry> next();
+
+private:
+	std::unique_ptr<VolumeIterator> m_volume_iterator;
+	std::unique_ptr<RecursiveFileIterator> m_file_iterator;
+
+	void try_retrieve_next();
+};
