167e98ce04dff49b15ec247caa040438145bdba9
Gil Alpert <alpertgil@gmail.com>
2025-02-11 23:31:21 +0200

    add more throwing functions to SimpleDll for testing

--- Apricot/SimpleDll/DllMain.cpp
+++ Apricot/SimpleDll/DllMain.cpp
@@ -1,4 +1,10 @@
-﻿#include <Windows.h>
+﻿#include "Exception.hpp"
+#include "Trace.hpp"
+#include "Protections/EntryPointProtector.hpp"
+
+#include <Windows.h>
+
+class Exception;
 
 BOOL WINAPI DllMain([[maybe_unused]] HINSTANCE instance,
                     [[maybe_unused]] const DWORD reason,
@@ -13,6 +19,44 @@ BOOL WINAPI DllMain([[maybe_unused]] HINSTANCE instance,
 		MessageBoxW(nullptr, L"SimpleDll unloaded", L"SimpleDll", 0);
 	}
 	return TRUE;
+}
+
+void throwing_function()
+{
+	try
+	{
+		Protections::EntryPointProtector protector;
+		TRACE(L"in throwing function ");
+		throw WinApiException(ErrorCode::UNKNOWN_ERROR);
+	}
+	catch (const Exception& exception)
+	{
+		TRACE(L"caught exception with code: ", exception.code());
+		MessageBoxW(nullptr, L"SimpleDll::throwing_function", L"SimpleDll", 0);
+	}
+}
+
+void throwing_uncaught_function()
+{
+	throw Exception(ErrorCode::UNKNOWN_ERROR);
+}
+
+void throwing_uncaught_seh_function()
+{
+	uint8_t* ptr = nullptr;
+	*ptr = 1;
+}
+
+void throwing_seh_function()
+{
+	__try
+	{
+		uint8_t* ptr = nullptr;
+		*ptr = 1;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
 }
 
 void exported_function_by_name()
