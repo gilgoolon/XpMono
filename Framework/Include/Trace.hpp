b5a70a754b526b88971efeffd08bf258a89aaa88
Gil Alpert <alpertgil@gmail.com>
2025-02-14 15:25:30 +0200

    include _DEBUG ifdef on TRACE

--- Framework/Include/Trace.hpp
+++ Framework/Include/Trace.hpp
@@ -8,6 +8,8 @@
 #define __PROJECTW__ L"???"
 #endif
 
+#ifdef _DEBUG
+
 #define TRACE(...) \
     try { \
         debug(Formatting::format(L"[" __PROJECTW__ L"] ", std::filesystem::path(__FILE__).filename().wstring(), ":", __LINE__, " " ,__FUNCTION__, " - ", __VA_ARGS__, L"\n")); \
@@ -15,3 +17,11 @@
 	{debug(L"failed to trace");};
 
 #define TRACE_S(str) debug(L"[" __PROJECTW__ L"] " __FILEW__ L":" __FUNCTIONW__ L" - " str);
+
+#else
+
+#define TRACE(...) ;
+
+#define TRACE_S(str) ;
+
+#endif
