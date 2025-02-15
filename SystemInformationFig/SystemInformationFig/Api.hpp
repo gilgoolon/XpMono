51a1674e225054568ecd1fbfff242d19a295c14c
Gil Alpert <alpertgil@gmail.com>
2025-02-15 18:07:12 +0200

    organize and introduce Api ns on SystemInformationFig

--- SystemInformationFig/SystemInformationFig/Api.hpp
+++ SystemInformationFig/SystemInformationFig/Api.hpp
@@ -3,15 +3,16 @@
 
 namespace Api
 {
-static constexpr Fig::FigId FIG_ID = 2;
+static constexpr Fig::FigId FIG_ID = 1;
 static constexpr Fig::VersionMajor VERSION_MAJOR = 1;
 static constexpr Fig::VersionMinor VERSION_MINOR = 0;
 
 enum class OperationType : Fig::OperationType
 {
 	RESERVED = 0,
-	DIRLIST,
-	DROP_FILE,
-	GET_FILE,
+	OS_INFO,
+	USERS_INFO,
+	PHYSICAL_DRIVES_INFO,
+	BIOS_INFO,
 };
 }
