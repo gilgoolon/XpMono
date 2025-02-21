cd09efde6938879faedd99e8a960eedbf6e85874
Gil Alpert <alpertgil@gmail.com>
2025-02-21 17:20:53 +0200

    new Liver projects

---
+++ Liver/LiverLib/Include/Liver.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Synchronization/Event.hpp"
+#include "Utils/Buffer.hpp"
+
+#include <memory>
+
+class Liver final
+{
+public:
+	explicit Liver();
+	~Liver() = default;
+	Liver(const Liver&) = delete;
+	Liver& operator=(const Liver&) = delete;
+	Liver(Liver&&) = delete;
+	Liver& operator=(Liver&&) = delete;
+
+private:
+	void run();
+
+public:
+	[[nodiscard]] static std::unique_ptr<Liver> create(const Buffer& liver_configuration);
+	static void main(const Buffer& liver_configuration);
+	[[nodiscard]] static std::wstring quit_event_name();
+
+private:
+	std::shared_ptr<Event> m_quit_event;
+};
---
+++ Liver/LiverLib/Liver.cpp
@@ -0,0 +1,31 @@
+﻿#include "Include/Liver.hpp"
+
+#include "Trace.hpp"
+#include "Synchronization/Event.hpp"
+
+Liver::Liver():
+	m_quit_event(std::make_shared<Event>(quit_event_name(), Event::Type::MANUAL_RESET))
+{
+}
+
+void Liver::run()
+{
+	TRACE(L"running");
+}
+
+std::unique_ptr<Liver> Liver::create([[maybe_unused]] const Buffer& liver_configuration)
+{
+	return std::make_unique<Liver>();
+}
+
+void Liver::main(const Buffer& liver_configuration)
+{
+	const std::unique_ptr<Liver> liver = create(liver_configuration);
+	liver->run();
+}
+
+std::wstring Liver::quit_event_name()
+{
+	static constexpr std::wstring_view QUIT_EVENT_NAME = L"LiverEvent";
+	return std::wstring{Event::GLOBAL_NAMESPACE} + std::wstring{QUIT_EVENT_NAME};
+}
