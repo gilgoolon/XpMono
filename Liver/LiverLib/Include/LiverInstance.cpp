d6cfc4303b19308448d79d457e9339395e9906b6
Gil Alpert <alpertgil@gmail.com>
2025-02-28 20:06:11 +0200

    generate GUIDs for Release names

--- Liver/LiverLib/Include/LiverInstance.cpp
+++ Liver/LiverLib/Include/LiverInstance.cpp
@@ -2,7 +2,12 @@
 
 #include "Synchronization/Event.hpp"
 
-static constexpr auto MUTEX_NAME = L"LiverMutex";
+static constexpr auto MUTEX_NAME =
+#ifdef _DEBUG
+	L"LiverMutex";
+#else
+	L"{95F97CCC-4DCD-4D38-8744-F31636E20A62}"
+#endif
 
 LiverInstance::LiverInstance():
 	m_mutex(Event::GLOBAL_NAMESPACE + std::wstring{MUTEX_NAME})
