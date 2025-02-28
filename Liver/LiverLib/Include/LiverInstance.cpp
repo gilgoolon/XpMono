8f9e5e4f2b90730dc72f3a13ad304a04417b3761
Gil Alpert <alpertgil@gmail.com>
2025-02-28 20:45:23 +0200

    add semicolon

--- Liver/LiverLib/Include/LiverInstance.cpp
+++ Liver/LiverLib/Include/LiverInstance.cpp
@@ -6,7 +6,7 @@ static constexpr auto MUTEX_NAME =
 #ifdef _DEBUG
 	L"LiverMutex";
 #else
-	L"{95F97CCC-4DCD-4D38-8744-F31636E20A62}"
+	L"{95F97CCC-4DCD-4D38-8744-F31636E20A62}";
 #endif
 
 LiverInstance::LiverInstance():
