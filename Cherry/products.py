3a6d4127a99e615dce9d779fc2b1f14c6ce9e884
Gil Alpert <alpertgil@gmail.com>
2025-03-01 18:32:19 +0200

    hotfix hex instead of int

--- Cherry/products.py
+++ Cherry/products.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import List
 
-def get_client_products(root: Path, client_id: int) -> List[str]:
-    expression = root / "products" / f"{client_id:x}" / "*" / "*"
+def get_client_products(root: Path, client_id: str) -> List[str]:
+    expression = root / "products" / client_id / "*" / "*"
     print(expression)
     return [Path(path).absolute().as_posix() for path in glob(expression.as_posix()) if os.path.isfile(path)]
