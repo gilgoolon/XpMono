ba156e13ef5fd5d6172a7e4cd6cb4b3fc83a8b39
Gil Alpert <alpertgil@gmail.com>
2025-03-01 11:30:23 +0200

    UnloadDllCommand struct

---
+++ Liver/LiverLib/Commands/UnloadDllCommand.cpp
@@ -0,0 +1,12 @@
+﻿#include "Commands/UnloadDllCommand.hpp"
+
+UnloadDllCommand::UnloadDllCommand(const Id id, const LibrariesContainer::LibraryId library_id):
+	ICommand(id),
+	m_library_id(library_id)
+{
+}
+
+ICommand::Type UnloadDllCommand::type() const
+{
+	return Type::UNLOAD_DLL;
+}
---
+++ Liver/LiverLib/Include/Commands/UnloadDllCommand.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "ICommand.hpp"
+#include "LibrariesContainer.hpp"
+
+class UnloadDllCommand final : public ICommand
+{
+public:
+	explicit UnloadDllCommand(Id id, LibrariesContainer::LibraryId library_id);
+	~UnloadDllCommand() override = default;
+	UnloadDllCommand(const UnloadDllCommand&) = delete;
+	UnloadDllCommand& operator=(const UnloadDllCommand&) = delete;
+	UnloadDllCommand(UnloadDllCommand&&) = delete;
+	UnloadDllCommand& operator=(UnloadDllCommand&&) = delete;
+
+	[[nodiscard]] Type type() const override;
+
+private:
+	LibrariesContainer::LibraryId m_library_id;
+};
