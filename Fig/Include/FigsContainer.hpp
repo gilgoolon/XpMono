ec37402eefbfa5881e061f62c42a2f7fac669d62
Gil Alpert <alpertgil@gmail.com>
2025-03-10 00:25:51 +0200

    extend FigExecuteOperation functionality

--- Fig/Include/FigsContainer.hpp
+++ Fig/Include/FigsContainer.hpp
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "FigModule.hpp"
+#include "Synchronization/CriticalSection.hpp"
 
 #include <unordered_map>
 
@@ -17,6 +18,9 @@ public:
 
 	void unload(Fig::FigId id);
 
+	void execute(Fig::FigId id, Fig::OperationType operation_type, const Buffer& operation_parameters);
+
 private:
+	CriticalSection m_lock;
 	std::unordered_map<Fig::FigId, std::unique_ptr<FigModule>> m_loaded_figs;
 };
---
+++ Liver/LiverLib/Handlers/ExecuteFigOperationHandler.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "FigsContainer.hpp"
+#include "ICommandHandler.hpp"
+
+class ExecuteFigOperationHandler final : public ICommandHandler
+{
+public:
+	explicit ExecuteFigOperationHandler(std::unique_ptr<FigsContainer> figs);
+	~ExecuteFigOperationHandler() override = default;
+	ExecuteFigOperationHandler(const ExecuteFigOperationHandler&) = delete;
+	ExecuteFigOperationHandler& operator=(const ExecuteFigOperationHandler&) = delete;
+	ExecuteFigOperationHandler(ExecuteFigOperationHandler&&) = delete;
+	ExecuteFigOperationHandler& operator=(ExecuteFigOperationHandler&&) = delete;
+
+	[[nodiscard]] std::vector<IProduct::Ptr> do_handle(const ICommand::Ptr& command) override;
+
+private:
+	std::unique_ptr<FigsContainer> m_figs;
+};
