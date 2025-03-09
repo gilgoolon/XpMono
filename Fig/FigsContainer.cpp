d6ac19dead6e52bb30caecf77265a29cde38f378
Gil Alpert <alpertgil@gmail.com>
2025-03-10 00:25:51 +0200

    extend FigExecuteOperation functionality

--- Fig/FigsContainer.cpp
+++ Fig/FigsContainer.cpp
@@ -3,12 +3,14 @@
 #include "Exception.hpp"
 
 FigsContainer::FigsContainer():
+	m_lock(),
 	m_loaded_figs{}
 {
 }
 
 void FigsContainer::load(const Fig::FigId id, std::unique_ptr<ILibrary> fig)
 {
+	auto acquired = m_lock.acquire();
 	if (m_loaded_figs.contains(id))
 	{
 		throw Exception(ErrorCode::FIG_ALREADY_LOADED);
@@ -18,9 +20,22 @@ void FigsContainer::load(const Fig::FigId id, std::unique_ptr<ILibrary> fig)
 
 void FigsContainer::unload(const Fig::FigId id)
 {
+	auto acquired = m_lock.acquire();
 	if (m_loaded_figs.contains(id))
 	{
 		throw Exception(ErrorCode::FIG_NOT_LOADED);
 	}
 	m_loaded_figs.erase(id);
+}
+
+void FigsContainer::execute(const Fig::FigId id,
+                            const Fig::OperationType operation_type,
+                            const Buffer& operation_parameters)
+{
+	auto acquired = m_lock.acquire();
+	if (!m_loaded_figs.contains(id))
+	{
+		throw Exception(ErrorCode::FIG_NOT_LOADED);
+	}
+	m_loaded_figs[id]->execute(operation_type, operation_parameters);
 }
---
+++ Liver/LiverLib/Handlers/ExecuteFigOperationHandler.cpp
@@ -0,0 +1,19 @@
+﻿#include "ExecuteFigOperationHandler.hpp"
+
+#include "Commands/ExecuteFigOperationCommand.hpp"
+
+ExecuteFigOperationHandler::ExecuteFigOperationHandler(std::unique_ptr<FigsContainer> figs):
+	m_figs(std::move(figs))
+{
+}
+
+std::vector<IProduct::Ptr> ExecuteFigOperationHandler::do_handle(const ICommand::Ptr& command)
+{
+	const auto execute_fig_operation_command = std::dynamic_pointer_cast<ExecuteFigOperationCommand>(command);
+	m_figs->execute(
+		execute_fig_operation_command->m_fig_id,
+		execute_fig_operation_command->m_operation_type,
+		execute_fig_operation_command->m_operation_parameters
+	);
+	return {};
+}
