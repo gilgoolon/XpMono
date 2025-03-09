02aa21eb31a372fc1dd0375d3d1435ca281ff0a4
Gil Alpert <alpertgil@gmail.com>
2025-03-10 00:12:14 +0200

    parse operation

--- Liver/LiverLib/Include/Commands/ExecuteFigOperationCommand.hpp
+++ Liver/LiverLib/Include/Commands/ExecuteFigOperationCommand.hpp
@@ -1,11 +1,15 @@
 ﻿#pragma once
 #include "FigApi.hpp"
 #include "Commands/ICommand.hpp"
+#include "Utils/Buffer.hpp"
 
 class ExecuteFigOperationCommand final : public ICommand
 {
 public:
-	explicit ExecuteFigOperationCommand(Id command_id, Fig::FigId fig_id, Fig::OperationType operation_type);
+	explicit ExecuteFigOperationCommand(Id command_id,
+	                                    Fig::FigId fig_id,
+	                                    Fig::OperationType operation_type,
+	                                    Buffer operation_parameters);
 	~ExecuteFigOperationCommand() override = default;
 	ExecuteFigOperationCommand(const ExecuteFigOperationCommand&) = delete;
 	ExecuteFigOperationCommand& operator=(const ExecuteFigOperationCommand&) = delete;
@@ -14,7 +18,10 @@ public:
 
 	[[nodiscard]] Type type() const override;
 
+	friend class ExecuteFigOperationHandler;
+
 private:
 	Fig::FigId m_fig_id;
 	Fig::OperationType m_operation_type;
+	Buffer m_operation_parameters;
 };
