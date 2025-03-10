15b13750b6da9c1d175d056692edb8e90f5a958c
Gil Alpert <alpertgil@gmail.com>
2025-03-10 22:43:11 +0200

    wait_for_all and wait_for_any in IWaitable

--- Framework/Synchronization/IWaitable.cpp
+++ Framework/Synchronization/IWaitable.cpp
@@ -30,3 +30,56 @@ void IWaitable::sleep(const Time::Duration duration)
 {
 	Sleep(Time::duration_cast<Time::Millis>(duration).count());
 }
+
+WaitResult IWaitable::wait_for_any(const std::vector<std::shared_ptr<IWaitable>>& objects, const Time::Duration timeout)
+{
+	static constexpr bool WAIT_FOR_ANY = false;
+	return wait_for_multiple(objects, timeout, WAIT_FOR_ANY);
+}
+
+WaitResult IWaitable::wait_for_all(const std::vector<std::shared_ptr<IWaitable>>& objects, const Time::Duration timeout)
+{
+	static constexpr bool WAIT_FOR_ALL = true;
+	return wait_for_multiple(objects, timeout, WAIT_FOR_ALL);
+}
+
+WaitResult IWaitable::wait_for_multiple(const std::vector<std::shared_ptr<IWaitable>>& objects,
+                                        const Time::Duration timeout,
+                                        const bool wait_all)
+{
+	std::vector<HANDLE> handles;
+	handles.reserve(objects.size());
+	for (const std::shared_ptr<IWaitable>& object : objects)
+	{
+		handles.push_back(object->handle());
+	}
+	const BOOL wait_all_win = wait_all ? TRUE : FALSE;
+
+	const DWORD result = WaitForMultipleObjects(
+		handles.size(),
+		handles.data(),
+		wait_all_win,
+		Time::duration_cast<Time::Millis>(timeout).count()
+	);
+
+	if (result == WAIT_TIMEOUT)
+	{
+		return {.status = WaitStatus::TIMEOUT, .triggered_object = std::nullopt};
+	}
+
+	if (result >= WAIT_OBJECT_0 && result <= WAIT_OBJECT_0 + handles.size())
+	{
+		if (wait_all)
+		{
+			return {.status = WaitStatus::FINISHED, .triggered_object = std::nullopt};
+		}
+		return {.status = WaitStatus::FINISHED, .triggered_object = result - WAIT_OBJECT_0};
+	}
+
+	if (result >= WAIT_ABANDONED_0 && result <= WAIT_ABANDONED_0 + handles.size())
+	{
+		return {.status = WaitStatus::OBJECT_CLOSED, .triggered_object = result - WAIT_ABANDONED_0};
+	}
+
+	return {.status = WaitStatus::FAILED, .triggered_object = std::nullopt};
+}
