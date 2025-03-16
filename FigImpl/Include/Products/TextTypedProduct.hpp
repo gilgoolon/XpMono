cded00d137afd97b4501c3395b063ac72d33448f
Gil Alpert <alpertgil@gmail.com>
2025-03-16 19:40:28 +0200

    TextTypedProduct

---
+++ FigImpl/Include/Products/TextTypedProduct.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Products/TypedProduct.hpp"
+
+#include <string>
+
+class TextTypedProduct final : public TypedProduct
+{
+public:
+	explicit TextTypedProduct(std::wstring text);
+	~TextTypedProduct() override = default;
+	TextTypedProduct(const TextTypedProduct&) = delete;
+	TextTypedProduct& operator=(const TextTypedProduct&) = delete;
+	TextTypedProduct(TextTypedProduct&&) = delete;
+	TextTypedProduct& operator=(TextTypedProduct&&) = delete;
+
+	[[nodiscard]] Type type() const override;
+	[[nodiscard]] Buffer data() const override;
+
+private:
+	std::wstring m_text;
+};
