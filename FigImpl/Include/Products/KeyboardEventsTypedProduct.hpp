7e4053202bec48afa7e62b06fb68d9f2b1931958
Gil Alpert <alpertgil@gmail.com>
2025-03-25 22:02:08 +0200

    KeyboardEventsTypedProduct

---
+++ FigImpl/Include/Products/KeyboardEventsTypedProduct.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "TypedProduct.hpp"
+#include "../../../KeyCollectorFig/KeyCollectorFig/Operations/KeyboardEvent.hpp"
+
+class KeyboardEventsTypedProduct final : public TypedProduct
+{
+public:
+	explicit KeyboardEventsTypedProduct(KeyboardEvents keyboard_events);
+	~KeyboardEventsTypedProduct() override = default;
+	KeyboardEventsTypedProduct(const KeyboardEventsTypedProduct&) = delete;
+	KeyboardEventsTypedProduct& operator=(const KeyboardEventsTypedProduct&) = delete;
+	KeyboardEventsTypedProduct(KeyboardEventsTypedProduct&&) = delete;
+	KeyboardEventsTypedProduct& operator=(KeyboardEventsTypedProduct&&) = delete;
+
+	[[nodiscard]] Type type() const override;
+	[[nodiscard]] Buffer data() const override;
+
+private:
+	KeyboardEvents m_keyboard_events;
+};
