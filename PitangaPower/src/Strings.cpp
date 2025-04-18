a4e277d6323c4253b7c0107cde09a8666cbc740a
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-04-18 19:34:38 +0300

    PitangaPower: String as_buffer

--- PitangaPower/src/Strings.cpp
+++ PitangaPower/src/Strings.cpp
@@ -5,6 +5,11 @@ std::string Strings::as_string(const std::vector<uint8_t> &buffer)
     return std::string{reinterpret_cast<const char*>(buffer.data()), reinterpret_cast<const char*>(buffer.data() + buffer.size())};
 }
 
+std::vector<uint8_t> Strings::as_buffer(const std::string &string)
+{
+    return std::vector<uint8_t>{reinterpret_cast<const uint8_t *>(string.data()), reinterpret_cast<const uint8_t *>(string.data() + string.size())};
+}
+
 std::vector<std::string> Strings::split(const std::string &str, const char delimiter)
 {
     std::vector<std::string> result;
--- PitangaPower/src/Strings.hpp
+++ PitangaPower/src/Strings.hpp
@@ -6,6 +6,7 @@
 namespace Strings
 {
     std::string as_string(const std::vector<uint8_t>& buffer);
+    std::vector<uint8_t> as_buffer(const std::string& string);
 
     std::vector<std::string> split(const std::string& str, const char delimiter);
 }
