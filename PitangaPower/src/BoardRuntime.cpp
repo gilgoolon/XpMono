5e2cd4021d223b6e1d8132972cd6c887b1544d6a
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-04-18 19:35:52 +0300

    PitangaPower: call pico-vfs::fs_init() in BoardRuntime

--- PitangaPower/src/BoardRuntime.cpp
+++ PitangaPower/src/BoardRuntime.cpp
@@ -1,10 +1,16 @@
 #include "BoardRuntime.hpp"
 
-#include "bsp/board.h"
-#include "tusb.h"
+#include <bsp/board.h>
+#include <tusb.h>
+#include <filesystem/vfs.h>
+#include <stdexcept>
 
 BoardRuntime::BoardRuntime()
 {
     board_init();
     tusb_init();
+    if (!fs_init())
+    {
+        throw std::runtime_error("failed to init filesystem");
+    }
 }
