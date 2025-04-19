62fd309e9fb429e93376e2f11dc06099044d26fa
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-04-19 14:48:45 +0300

    PitangaPower: use internal path from Config ns

--- PitangaPower/src/BoardFilesystem.cpp
+++ PitangaPower/src/BoardFilesystem.cpp
@@ -1,9 +1,11 @@
 #include "BoardFilesystem.hpp"
 
+#include "Config.hpp"
+
 BoardFilesystem::BoardFilesystem() : m_flash_block_device(1 * 1024 * 1024, 512 * 1024),
                                      m_flash_internal_device(1 * 1024 * 1024 + 512 * 1024, 0),
                                      m_internal_filesystem(std::make_unique<LittlefsFilesystem>()),
                                      m_external_filesystem(std::make_unique<FatFilesystem>()),
-                                     m_internal_mount(m_flash_internal_device.mount("/internal", m_internal_filesystem))
+                                     m_internal_mount(m_flash_internal_device.mount(Config::INTERNAL_FILESYSTEM_ROOT, m_internal_filesystem))
 {
 }
--- PitangaPower/src/Config.hpp
+++ PitangaPower/src/Config.hpp
@@ -3,4 +3,5 @@
 namespace Config
 {
     static constexpr const char* PAYLOAD_PATH = "payload.json";
+    static constexpr const char* INTERNAL_FILESYSTEM_ROOT = "/internal";
 }
