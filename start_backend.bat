9989d4fb785a6ee69a63bc689d9efc113ab8d9ed
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-06-17 21:45:28 +0300

    Environment: remove annoying hang from start scripts

--- start_backend.bat
+++ start_backend.bat
@@ -1,12 +1,6 @@
 @echo off
 
-REM Start each Python app in the background
 start "" python -m Cherry &
 start "" python -m CornCake &
 start "" python -m PassionFruit &
 start "" npm start --prefix PassionFruit/frontend &
-
-REM Wait indefinitely (optional: or you could wait for a specific process or log)
-:loop
-timeout /t 60 > nul
-goto loop
