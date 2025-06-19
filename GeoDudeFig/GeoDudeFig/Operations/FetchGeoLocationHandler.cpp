838131020ddf3bed6f2bd40ba3a548de5441b75e
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-06-19 21:25:41 +0300

    GeoDude: prepare FetchGeoLocationHandler skeleton for calling API from Liver with public IP precision/fallback

---
+++ GeoDudeFig/GeoDudeFig/Operations/FetchGeoLocationHandler.cpp
@@ -0,0 +1,49 @@
+﻿#include "FetchGeoLocationHandler.hpp"
+
+#include "Utils/Formatting.hpp"
+
+namespace Parameters
+{
+static constexpr auto API_KEY = "api_key";
+}
+
+FetchGeoLocationHandler::FetchGeoLocationHandler(std::unique_ptr<Event> operation_event, std::string api_key):
+	IOperationHandler(std::move(operation_event)),
+	m_api_key(std::move(api_key))
+
+{
+}
+
+FetchGeoLocationHandler::FetchGeoLocationHandler(std::unique_ptr<Event> operation_event, const Json& parameters):
+	FetchGeoLocationHandler(std::move(operation_event), parameters[Parameters::API_KEY].get<std::string>())
+{
+}
+
+FetchGeoLocationHandler::FetchGeoLocationHandler(std::unique_ptr<Event> operation_event, const Buffer& raw_parameters):
+	FetchGeoLocationHandler(std::move(operation_event), Json::parse(Strings::to_string(raw_parameters)))
+{
+}
+
+std::wstring FetchGeoLocationHandler::Location::serialize() const
+{
+	const Formatting::Fields fields = {
+		{L"latitude", Strings::to_wstring(latitude)},
+		{L"longitude", Strings::to_wstring(longitude)},
+		{L"accuracy", Strings::to_wstring(accuracy)},
+	};
+
+	return Formatting::format_fields(fields);
+}
+
+std::wstring FetchGeoLocationHandler::format_location(const Location& location)
+{
+	return Strings::concat(
+		std::wstring{L"#Location"},
+		std::wstring{L"\n"},
+		location.serialize()
+	);
+}
+
+void FetchGeoLocationHandler::run()
+{
+}
---
+++ GeoDudeFig/GeoDudeFig/Operations/FetchGeoLocationHandler.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "Json.hpp"
+#include "Handlers/IOperationHandler.hpp"
+#include "Synchronization/Event.hpp"
+
+#include <memory>
+
+class FetchGeoLocationHandler final : public IOperationHandler
+{
+public:
+	explicit FetchGeoLocationHandler(std::unique_ptr<Event> operation_event, std::string api_key);
+	explicit FetchGeoLocationHandler(std::unique_ptr<Event> operation_event, const Json& parameters);
+	explicit FetchGeoLocationHandler(std::unique_ptr<Event> operation_event, const Buffer& raw_parameters);
+	~FetchGeoLocationHandler() override = default;
+	FetchGeoLocationHandler(const FetchGeoLocationHandler&) = delete;
+	FetchGeoLocationHandler& operator=(const FetchGeoLocationHandler&) = delete;
+	FetchGeoLocationHandler(FetchGeoLocationHandler&&) = delete;
+	FetchGeoLocationHandler& operator=(FetchGeoLocationHandler&&) = delete;
+
+private:
+	struct Location final
+	{
+		double latitude;
+		double longitude;
+		double accuracy;
+
+		[[nodiscard]] std::wstring serialize() const;
+	};
+
+	[[nodiscard]] static std::wstring format_location(const Location& location);
+
+public:
+	void run() override;
+
+private:
+	std::string m_api_key;
+};
