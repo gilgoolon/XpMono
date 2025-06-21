ad453ec2fc040dc64e26180f0517e5e771adf272
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-06-21 22:34:35 +0300

    GoonGaloreFig: create and personalize fig

---
+++ GoonGaloreFig/GoonGaloreFig/Operations/EnumerateMediaDevicesHandler.cpp
@@ -0,0 +1,10 @@
+﻿#include "EnumerateMediaDevicesHandler.hpp"
+
+EnumerateMediaDevicesHandler::EnumerateMediaDevicesHandler(std::unique_ptr<Event> operation_event):
+	IOperationHandler(std::move(operation_event))
+{
+}
+
+void EnumerateMediaDevicesHandler::run()
+{
+}
---
+++ GoonGaloreFig/GoonGaloreFig/Operations/EnumerateMediaDevicesHandler.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Json.hpp"
+#include "Handlers/IOperationHandler.hpp"
+#include "Synchronization/Event.hpp"
+
+#include <memory>
+
+class EnumerateMediaDevicesHandler final : public IOperationHandler
+{
+public:
+	explicit EnumerateMediaDevicesHandler(std::unique_ptr<Event> operation_event);
+	~EnumerateMediaDevicesHandler() override = default;
+	EnumerateMediaDevicesHandler(const EnumerateMediaDevicesHandler&) = delete;
+	EnumerateMediaDevicesHandler& operator=(const EnumerateMediaDevicesHandler&) = delete;
+	EnumerateMediaDevicesHandler(EnumerateMediaDevicesHandler&&) = delete;
+	EnumerateMediaDevicesHandler& operator=(EnumerateMediaDevicesHandler&&) = delete;
+
+	void run() override;
+};
