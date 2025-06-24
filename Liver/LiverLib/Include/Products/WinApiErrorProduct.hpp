b78c15ec8c8adf0d27fd6af3e07f3504ee01cbb4
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-06-24 12:53:16 +0300

    Ignore: exclude Products sources from gitignore

---
+++ Liver/LiverLib/Include/Products/WinApiErrorProduct.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "ErrorProduct.hpp"
+
+class WinApiErrorProduct final : public ErrorProduct
+{
+public:
+	explicit WinApiErrorProduct(const ICommand::Ptr& command, ErrorCode error_code, uint32_t win_api_error);
+	~WinApiErrorProduct() override = default;
+	WinApiErrorProduct(const WinApiErrorProduct&) = delete;
+	WinApiErrorProduct& operator=(const WinApiErrorProduct&) = delete;
+	WinApiErrorProduct(WinApiErrorProduct&&) = delete;
+	WinApiErrorProduct& operator=(WinApiErrorProduct&&) = delete;
+
+	[[nodiscard]] Type type() const override;
+	[[nodiscard]] Buffer data() const override;
+
+private:
+	uint32_t m_win_api_error;
+};
---
+++ Liver/LiverLib/Products/WinApiErrorProduct.cpp
@@ -0,0 +1,21 @@
+﻿#include "Products/WinApiErrorProduct.hpp"
+
+#include "Utils/Strings.hpp"
+
+WinApiErrorProduct::WinApiErrorProduct(const ICommand::Ptr& command,
+                                       const ErrorCode error_code,
+                                       const uint32_t win_api_error):
+	ErrorProduct(command, error_code),
+	m_win_api_error(win_api_error)
+{
+}
+
+IProduct::Type WinApiErrorProduct::type() const
+{
+	return Type::WIN_API_ERROR_PRODUCT;
+}
+
+Buffer WinApiErrorProduct::data() const
+{
+	return Strings::concat(ErrorProduct::data(), as_buffer(m_win_api_error));
+}
